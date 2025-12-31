7794b6c853075e458639258c8309083ee424e629
yikai <yikai@didiglobal.com>
2025-12-31 18:18:13 +0800

    修改路径

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  // 核心两行
+  base: '/react-virtual-list/',
+  publicPath: '/react-virtual-list/',
   themeConfig: {
     name: 'react-virtual-list',
   },
